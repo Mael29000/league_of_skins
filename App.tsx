679c9b7f558c1f8059deba85f8736556b6def84d
Mael29000 <maelsuard@orange.fr>
2023-06-10 00:19:34 +0200

    Fix keyboard reducing image size

--- App.tsx
+++ App.tsx
@@ -7,55 +7,66 @@ import { GameProvider } from "./src/context/GameContext";
 import GameOver from "./src/pages/GameOver";
 import Victory from "./src/pages/Victory";
 import Disclaimer from "./src/pages/Disclaimer";
+import { View, Dimensions } from "react-native";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+    const Stack = createNativeStackNavigator();
 
-  // load the font before rendering the app
-  const [loaded] = useFonts({
-    "Montserrat-Black": require("./assets/fonts/Montserrat-Black.ttf"),
-    "Montserrat-BlackItalic": require("./assets/fonts/Montserrat-BlackItalic.ttf"),
-    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
-    "Montserrat-BoldItalic": require("./assets/fonts/Montserrat-BoldItalic.ttf"),
-    "Montserrat-ExtraBold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
-    "Montserrat-ExtraBoldItalic": require("./assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
-    "Montserrat-ExtraLight": require("./assets/fonts/Montserrat-ExtraLight.ttf"),
-    "Montserrat-ExtraLightItalic": require("./assets/fonts/Montserrat-ExtraLightItalic.ttf"),
-    "Montserrat-Italic": require("./assets/fonts/Montserrat-Italic.ttf"),
-    "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
-    "Montserrat-LightItalic": require("./assets/fonts/Montserrat-LightItalic.ttf"),
-    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
-    "Montserrat-MediumItalic": require("./assets/fonts/Montserrat-MediumItalic.ttf"),
-    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
-    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
-    "Montserrat-SemiBoldItalic": require("./assets/fonts/Montserrat-SemiBoldItalic.ttf"),
-    "Montserrat-Thin": require("./assets/fonts/Montserrat-Thin.ttf"),
-    "Montserrat-ThinItalic": require("./assets/fonts/Montserrat-ThinItalic.ttf"),
-  });
+    // load the font before rendering the app
+    const [loaded] = useFonts({
+        "Montserrat-Black": require("./assets/fonts/Montserrat-Black.ttf"),
+        "Montserrat-BlackItalic": require("./assets/fonts/Montserrat-BlackItalic.ttf"),
+        "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+        "Montserrat-BoldItalic": require("./assets/fonts/Montserrat-BoldItalic.ttf"),
+        "Montserrat-ExtraBold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+        "Montserrat-ExtraBoldItalic": require("./assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
+        "Montserrat-ExtraLight": require("./assets/fonts/Montserrat-ExtraLight.ttf"),
+        "Montserrat-ExtraLightItalic": require("./assets/fonts/Montserrat-ExtraLightItalic.ttf"),
+        "Montserrat-Italic": require("./assets/fonts/Montserrat-Italic.ttf"),
+        "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
+        "Montserrat-LightItalic": require("./assets/fonts/Montserrat-LightItalic.ttf"),
+        "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+        "Montserrat-MediumItalic": require("./assets/fonts/Montserrat-MediumItalic.ttf"),
+        "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+        "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+        "Montserrat-SemiBoldItalic": require("./assets/fonts/Montserrat-SemiBoldItalic.ttf"),
+        "Montserrat-Thin": require("./assets/fonts/Montserrat-Thin.ttf"),
+        "Montserrat-ThinItalic": require("./assets/fonts/Montserrat-ThinItalic.ttf"),
+    });
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  const screenOptions = {
-    headerShown: false,
-    unmountOnBlur: true,
-  };
+    const screenOptions = {
+        headerShown: false,
+        unmountOnBlur: true,
+    };
 
-  return (
-    <GameProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={screenOptions}
-          initialRouteName="Disclaimer"
-        >
-          <Stack.Screen name="Disclaimer" component={Disclaimer} />
-          <Stack.Screen name="Home" component={MainMenu} />
-          <Stack.Screen name="Game" component={Game} />
-          <Stack.Screen name="Game Over" component={GameOver} />
-          <Stack.Screen name="Victory" component={Victory} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </GameProvider>
-  );
+    return (
+        <GameProvider>
+            <View
+                style={{
+                    height: Dimensions.get("window").height,
+                    backgroundColor: "black",
+                }}
+            >
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={screenOptions}
+                        initialRouteName="Disclaimer"
+                    >
+                        <Stack.Screen
+                            name="Disclaimer"
+                            component={Disclaimer}
+                        />
+                        <Stack.Screen name="Home" component={MainMenu} />
+                        <Stack.Screen name="Game" component={Game} />
+                        <Stack.Screen name="Game Over" component={GameOver} />
+                        <Stack.Screen name="Victory" component={Victory} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </View>
+        </GameProvider>
+    );
 }
