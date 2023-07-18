406cc01096cd26aa8f6dd2a1b932481d6b3b298a
Mael29000 <maelsuard@orange.fr>
2023-07-18 13:51:20 +0200

    Clean pause screen code

--- src/pages/Pause.tsx
+++ src/pages/Pause.tsx
@@ -2,52 +2,43 @@ import React from "react";
 import { ImageBackground, View } from "react-native";
 
 export default function Pause() {
-  const pauseImages = [
-    require("../../assets/backgrounds/pause_1.jpg"),
-    require("../../assets/backgrounds/pause_2.jpg"),
-    require("../../assets/backgrounds/pause_3.jpg"),
-    require("../../assets/backgrounds/pause_4.jpg"),
-    require("../../assets/backgrounds/pause_5.jpg"),
-  ];
-
-  const imageIndex = 2;
-
-  return (
-    <>
-      <View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: "black",
-          width: "100%",
-          height: "100%",
-        }}
-      />
-      <ImageBackground
-        source={pauseImages[imageIndex]}
-        style={{
-          height: "100%",
-          width: "100%",
-          position: "absolute",
-          display: "flex",
-          alignItems: "center",
-        }}
-      />
-      <View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: "rgba(0, 0, 0, 0.2)",
-          width: "100%",
-          height: "100%",
-        }}
-      />
-    </>
-  );
+    const pauseImage = require("../../assets/backgrounds/pause.jpg");
+    return (
+        <>
+            <View
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: "black",
+                    width: "100%",
+                    height: "100%",
+                }}
+            />
+            <ImageBackground
+                source={pauseImage}
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    position: "absolute",
+                    display: "flex",
+                    alignItems: "center",
+                }}
+            />
+            <View
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    width: "100%",
+                    height: "100%",
+                }}
+            />
+        </>
+    );
 }
